ba606b34571531ae34c8db5ec88ca1353e374d52
Shaarang Singh <shaarangg.singh@gmail.com>
2022-06-12 10:59:09 +0530

    Time: 4 ms (41.65%), Space: 8.4 MB (24.36%) - LeetHub

--- 75-sort-colors/75-sort-colors.cpp
+++ 75-sort-colors/75-sort-colors.cpp
@@ -2,28 +2,22 @@ class Solution {
 public:
     void sortColors(vector<int>& nums) {
         int n = nums.size();
-        unordered_map<int,int> dict;
-        dict[0]=0;
-        dict[1]=0;
-        dict[2]=0;
-        for(int i=0; i<n; i++){
-            dict[nums[i]]+=1;
-        }
-        int i=0;
-        while(dict[0]){
-            nums[i]=0;
-            dict[0]--;
-            i++;
-        }
-        while(dict[1]){
-            nums[i]=1;
-            dict[1]--;
-            i++;
-        }
-        while(dict[2]){
-            nums[i]=2;
-            dict[2]--;
-            i++;
+        int pos2 = n-1;
+        int pos1=0;
+        int pos0 = 0;
+        while(pos1<=pos2){
+            if(nums[pos1]==2){
+                swap(nums[pos1],nums[pos2]);
+                pos2--;
+            }
+            else if(nums[pos1]==1){
+                pos1++;
+            }
+            else if(nums[pos1]==0){
+                swap(nums[pos1],nums[pos0]);
+                pos0++;
+                pos1++;
+            }
         }
     }
 };
