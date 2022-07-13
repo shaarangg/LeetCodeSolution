a63b254cf6db3459a5793c44bd822701a05cdc41
Shaarang Singh <shaarangg.singh@gmail.com>
2022-07-13 22:50:17 +0530

    Time: 6 ms (13.64%), Space: 6.4 MB (8.90%) - LeetHub

---
+++ 28-implement-strstr/28-implement-strstr.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int strStr(string haystack, string needle) {
+        int n = haystack.length();
+        int m = needle.length();
+        if(m>n) return -1;
+        if(m==0) return 0;
+        for(int i=0; i<n-m+1; i++){
+            if(haystack.substr(i,m)==needle) return i;
+        }
+        return -1;
+    }
+};
