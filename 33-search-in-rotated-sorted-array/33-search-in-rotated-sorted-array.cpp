6e8b32655a338272ee0b223c53fb3c99a1c75be3
Shaarang Singh <shaarangg.singh@gmail.com>
2022-07-31 19:25:03 +0530

    Time: 12 ms (14.38%), Space: 11.1 MB (29.07%) - LeetHub

--- 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.cpp
+++ 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.cpp
@@ -1,29 +1,36 @@
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
-        int n = nums.size()-1;
-        int high=n,low=0;
-        int mid =0;
-        while(low<=high){
-            mid = low + (high-low)/2;
-            if(mid>low && nums[mid-1]>nums[mid]) break;
-            else if(mid<high && nums[mid+1]<nums[mid]){
-                mid+=1;
+        int n = nums.size();
+        int lo = 0, hi=n-1;
+        while(lo<hi){
+            int mid = lo + (hi-lo)/2;
+            if(mid!=0 && nums[mid]<nums[mid-1]){
+                lo=mid;
                 break;
             }
-            else if(nums[mid]>nums[high]) low = mid+1;
-            else high = mid-1;
+            else if(mid!=n-1 && nums[mid]>nums[mid+1]){
+                lo=mid+1;
+                break;
+            }
+            else if(nums[mid]<nums[lo]) hi = mid-1;
+            else if(nums[mid]>nums[hi]) lo=mid+1;
+            else break;
         }
-        cout<<mid;
-        high=n;
-        low=0;
-        if(target<=nums[high]) low = mid;
-        else high=mid-1;
-        while(low<=high){
-            int mid = low + (high-low)/2;
-            if(target==nums[mid]) return mid;
-            else if(target<nums[mid]) high=mid-1;
-            else low=mid+1;
+        cout<<lo;
+        if(target==nums[lo]) return lo;
+        if(target<=nums[n-1]){
+            hi=n-1;
+        }
+        else {
+            hi=lo-1;
+            lo=0;
+        }
+        while(lo<=hi){
+            int mid = lo + (hi-lo)/2;
+            if(nums[mid]==target) return mid;
+            else if(target>nums[mid]) lo=mid+1;
+            else hi=mid-1;
         }
         return -1;
     }
