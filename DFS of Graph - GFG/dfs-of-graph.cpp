05869487a761808c130269871ca310985c6ec749
Shaarang Singh <shaarangg.singh@gmail.com>
2022-10-27 11:42:03 +0530

    Added solution - LeetHub

---
+++ DFS of Graph - GFG/dfs-of-graph.cpp
@@ -0,0 +1,50 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution {
+  public:
+    unordered_map<int,bool> vis;
+    vector<int> ans;
+    void util(vector<int> adj[], int node){
+        if(!vis[node]){
+            vis[node]=true;
+            ans.push_back(node);
+            for(auto i: adj[node]) util(adj,i);
+        }
+    }
+    vector<int> dfsOfGraph(int V, vector<int> adj[]) {
+        util(adj,0);
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int tc;
+    cin >> tc;
+    while (tc--) {
+        int V, E;
+        cin >> V >> E;
+
+        vector<int> adj[V];
+
+        for (int i = 0; i < E; i++) {
+            int u, v;
+            cin >> u >> v;
+            adj[u].push_back(v);
+            adj[v].push_back(u);
+        }
+        // string s1;
+        // cin>>s1;
+        Solution obj;
+        vector<int> ans = obj.dfsOfGraph(V, adj);
+        for (int i = 0; i < ans.size(); i++) {
+            cout << ans[i] << " ";
+        }
+        cout << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
